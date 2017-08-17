51269db277214e059fe61736a8d9e0bfd062ea74
abohannon <abo46n2@gmail.com>
2017-08-17 12:09:04 -0700

    More styling of the board itself, started working on API call

--- src/index.js
+++ src/index.js
@@ -3,47 +3,105 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-
 class Board extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      value: ''
+      rank: '',
+      username: '',
+      points30: '',
+      pointsAll: ''
+
     };
   }
 
-  render() {
-    return (
+  componentWillMount() {
+    console.log('Component is mounting...');
+  }
 
-      <div className="row">
-        <div className="col 12">
-          <ul>
-            <BoardItem />
-          </ul>
+  // componentDidMount () {
+  //   return $.getJSON('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+  //   .then((data) => {
+  //     console.log(data.results);
+  //   });
+  // }
+
+  render() {
+
+    const BoardStyle = {
+      container: {
+        boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+      },
+
+      header: {
+        backgroundColor: "#3F60DA",
+        width: "100%",
+        color: "white",
+        height: "100px",
+        padding: "25px 15px 25px 15px",
+        boxShadow: "0 -1px 6px rgba(0,0,0,0.16), 0 -1px 6px rgba(0,0,0,0.23)"
+      },
+    };
+
+    return (
+      <div style={{marginTop: "5vh"}}>
+      <div className="container rounded-top boardHeader" style={BoardStyle.header}>
+        <h1>Freecodecamp Leaderboard</h1>
+      </div>
+      <div className="container rounded-bottom" style={BoardStyle.container}>
+        <div className="row">
+          <div className="col 12">
+
+              <Table/>
+          </div>
         </div>
       </div>
+    </div>
 
     );
   }
 
 }
 
-class BoardItem extends React.Component {
+class Table extends React.Component {
   render() {
     return (
-      <li className="collection-item avatar">
-      <img src="images/yuna.jpg" alt="" className="circle" />
-      <span className="title">Title</span>
-      <p>First Line <br />
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-  );
+
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Camper Name</th>
+            <th>Pts last 30 days</th>
+            <th>Pts all time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TableItem/>
+        </tbody>
+
+      </table>
+
+    );
   }
 }
 
+class TableItem extends React.Component {
+  render() {
+    return (
 
-ReactDOM.render(<Board />, document.getElementById('root'));
+      <tr>
+        <th scope="row">1</th>
+        <td>SkyCoder</td>
+        <td>314</td>
+        <td>2158</td>
+      </tr>
+
+    );
+  }
+}
+
+ReactDOM.render(
+  <Board/>, document.querySelector('#root'));
 registerServiceWorker();
